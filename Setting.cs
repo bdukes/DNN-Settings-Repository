4c4567ba795523c6d42af37759141da58fff3d93
Brian Dukes <bdukes@engagesoftware.com>
2014-09-04 12:44:30 -0500

    Documentation

--- Setting.cs
+++ Setting.cs
@@ -2,14 +2,23 @@
 
 namespace DotNetNuke.SettingsRepository
 {
+    /// <summary>The definition of a setting</summary>
+    /// <typeparam name="T">The type of the setting's value</typeparam>
     public struct Setting<T>
     {
+        /// <summary>Backing field for <see cref="SettingName"/></summary>
         private readonly string _settingName;
 
+        /// <summary>Backing field for <see cref="Scope"/></summary>
         private readonly SettingScope _scope;
 
+        /// <summary>Backing field for <see cref="DefaultValue"/></summary>
         private readonly T _defaultValue;
 
+        /// <summary>Initializes a new instance of the <see cref="Setting{T}"/> struct.</summary>
+        /// <param name="settingName">The name of the setting. This name needs to be sufficiently unique when using a <paramref name="scope"/> which isn't owned by the module (e.g. <see cref="SettingScope.Tab"/>, <see cref="SettingScope.Portal"/>, or <see cref="SettingScope.Host"/>)</param>
+        /// <param name="scope">The scope of the setting.</param>
+        /// <param name="defaultValue">The default value of the setting to use when the setting has not yet been set.</param>
         public Setting(string settingName, SettingScope scope, T defaultValue)
             : this()
         {
@@ -18,16 +27,19 @@ namespace DotNetNuke.SettingsRepository
             this._defaultValue = defaultValue;
         }
 
+        /// <summary>Gets the default value to use if the setting does not have a value set.</summary>
         public T DefaultValue
         {
             get { return this._defaultValue; }
         }
 
+        /// <summary>Gets the name of the setting. This name needs to be sufficiently unique when using a <see cref="Scope"/> which isn't owned by the module (e.g. <see cref="SettingScope.Tab"/>, <see cref="SettingScope.Portal"/>, or <see cref="SettingScope.Host"/>)</summary>
         public string SettingName
         {
             get { return this._settingName; }
         }
 
+        /// <summary>Gets the scope of the setting.</summary>
         public SettingScope Scope
         {
             get { return this._scope; }
--- SettingScope.cs
+++ SettingScope.cs
@@ -2,12 +2,22 @@
 
 namespace DotNetNuke.SettingsRepository
 {
+    /// <summary>The scope of a <see cref="Setting{T}"/></summary>
     public enum SettingScope
     {
-        TabModule = 1,
-        Module = 2,
-        Tab = 3,
-        Portal = 4,
-        Host = 5,
+        /// <summary>A setting that applied to an instance of a module on a particular page</summary>
+        TabModule = 0,
+
+        /// <summary>A setting that applies to a module instance (including on any page to which a reference of it is shared)</summary>
+        Module = 1,
+
+        /// <summary>A setting that applies to a page</summary>
+        Tab = 2,
+
+        /// <summary>A setting that applies to a site</summary>
+        Portal = 3,
+
+        /// <summary>A setting that applies to the entire DNN installation</summary>
+        Host = 4,
     }
 }
