edff8fd38f0f8dd899b271acea0d60c6dc91fed9
Brian Dukes <bdukes@engagesoftware.com>
2014-09-04 13:05:06 -0500

    Add tests for various scenarios getting Boolean values

--- DotNetNuke.SettingsRepository.UnitTests/SettingTests.vb
+++ DotNetNuke.SettingsRepository.UnitTests/SettingTests.vb
@@ -17,6 +17,39 @@ Public Class SettingsRepositoryTests
         Assert.IsTrue(settingValue)
     End Sub
 
+    <Test>
+    <TestCase("true", True)>
+    <TestCase("false", False)>
+    Public Sub GetValue_GivenStandardBooleanTrueText_ConvertsSettingValue(settingText As String, expectedValue As Boolean)
+        StubHostSettings(New Dictionary(Of String, String) From {{"MyModule_TheSetting", settingText}})
+        Dim setting As New Setting(Of Boolean)("MyModule_TheSetting", SettingScope.Host, False)
+        Dim repository As ISettingsRepository = New SettingsRepository(New ModuleInstanceContext())
+
+        Dim settingValue = repository.GetValue(setting)
+
+        Assert.AreEqual(expectedValue, settingValue)
+    End Sub
+
+    <Test>
+    Public Sub GetValue_GivenNoMatchingSetting_ReturnsDefaultSettingValue()
+        StubHostSettings(New Dictionary(Of String, String) From {{"MyModule_OtherSetting", "something"}})
+        Dim setting As New Setting(Of Boolean)("MyModule_TheSetting", SettingScope.Host, True)
+        Dim repository As ISettingsRepository = New SettingsRepository(New ModuleInstanceContext())
+
+        Dim settingValue = repository.GetValue(setting)
+
+        Assert.IsTrue(settingValue)
+    End Sub
+
+    <Test>
+    Public Sub GetValue_GivenInvalidBooleanText_ThrowsFormatException()
+        StubHostSettings(New Dictionary(Of String, String) From {{"MyModule_TheSetting", "not a boolean"}})
+        Dim setting As New Setting(Of Boolean)("MyModule_TheSetting", SettingScope.Host, False)
+        Dim repository As ISettingsRepository = New SettingsRepository(New ModuleInstanceContext())
+
+        Assert.Throws(Of FormatException)(Function() repository.GetValue(setting))
+    End Sub
+
     Private Sub StubHostSettings(settings As Dictionary(Of String, String))
         Dim stubHostController = Substitute.For(Of IHostController)()
         HostController.RegisterInstance(stubHostController)
